bf31cf63e56e8db1edbe20664ba24e312eef87bb
Lennart Betz <lennart.betz@netways.de>
2015-05-11 10:14:30 +0200

    added inline documentation

--- manifests/user.pp
+++ manifests/user.pp
@@ -1,3 +1,63 @@
+# == Define Resource: system::user
+#
+# Manage Users, their homedirs and ssh_authorized_keys.
+# Also between differnt users on the same machine
+#
+# == Parameters
+#
+# In the current version, please use the tag parameter to assign
+# the user's key to other users on the same machine.
+#
+# [*ensure*]
+#   present or absent
+#
+# [*user*]
+#   the user's account name, default sets to the title
+#
+# [*key*]
+#   the user's key, default to false for no key
+#
+# [*key_type*]
+#   and the key type, supported values:
+#     * ssh-dss (ssh-dsa)
+#     * ssh-rsa
+#     * ecdsa-sha2-nistp256
+#     * ecdsa-sha2-nistp256|ecdsa-sha2-nistp384
+#     * ecdsa-sha2-nistp521
+#
+# === Examples
+#
+# Creates users lbetz and tredel and stores their own keys in
+# their authorized_keys files. Additional the key of tredel is
+# saved to lbetz's authorized_keys file.
+#
+# system::user { 'lbetz':
+#   key => 'test1',
+# }
+#
+# system::user { 'tredel':
+#   key => 'test2',
+#   tag => ['lbetz'],
+# }
+#
+# Removes tredel including the key stored in lbetz's file.
+# Caution: For removing a user the tag and key parameter have
+# to set correctly, same vales are used for present.
+#
+# system::user { 'lbetz':
+#   key => 'test1',
+# }
+#
+# system::user { 'tredel':
+#   ensure => absent,
+#   key    => 'test2',
+#   tag    => ['lbetz'],
+# }
+#
+# === Author
+#
+# Author: Lennart betz <lennart.betz@netways.de>
+#
 define system::user(
   $ensure   = 'present',
   $user     = $title,
